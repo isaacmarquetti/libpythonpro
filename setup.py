d0445cdcdd3c9cb529fc5e22f81e42db36be33ab
Isaac Marquetti <isaac.marquetti@gmail.com>
2021-11-28 23:52:32 -0300

    Adicionando o setup.py
    close #6

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def find_package_data(
 
 
 PACKAGE = "libpythonpro"
-NAME = PACKAGE
+NAME = "libpythonpro_isaac"
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso Pytools"
 AUTHOR = "Isaac Marquetti"
 AUTHOR_EMAIL = "isaac.marquetti@gmail.com"
@@ -112,7 +112,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license='GNU Affero General Public Licence v3 or later (AGPLv3+)',
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
